816b5b92eeb23ece2ab7abfc791e6e79d5e597cf
isabela lacerda <ebdalmoro@gmail.com>
2023-04-10 07:55:30 -0300

    Create anafelix.php

---
+++ anafelix.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>linux</title>
+</head>
+<body>
+    <img src="https://i.pinimg.com/originals/a8/d8/cd/a8d8cd1903ed8d0b8aa20f398772210f.jpg">
+</body>
+</html>
