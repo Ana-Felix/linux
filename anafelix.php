82bd6fe07013a7b1337ff2a8ec443f2a67d3f365
isabela lacerda <92644868+Ana-Felix@users.noreply.github.com>
2023-04-11 08:31:49 -0300

    Update anafelix.php

--- anafelix.php
+++ anafelix.php
@@ -7,6 +7,38 @@
     <title>linux</title>
 </head>
 <body>
+
     <img src="https://i.pinimg.com/originals/a8/d8/cd/a8d8cd1903ed8d0b8aa20f398772210f.jpg">
 </body>
 </html>
+<?php
+
+/*** mysql hostname ***/
+$hostname = 'localhost';
+
+/*** mysql username ***/
+$username = 'username';
+
+/*** mysql password ***/
+$password = 'password';
+
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
+    /*** echo a message saying we have connected ***/
+    echo 'Connected to database';
+    }
+catch(PDOException $e)
+    {
+    echo $e->getMessage();
+    }
+?>
+<?php
+// Create connection
+$con=mysqli_connect(host,username,password,dbname); 
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?> 
